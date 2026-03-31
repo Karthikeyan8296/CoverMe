7e897552d6b870197acf77e94b423fe484a8074c
Karthik <karthikeyan.p@poshmark.com>
2026-03-31 14:44:55 +0530

    testing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,23 +21,5 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        //ADD THIS — needed so the plugin jar itself can be downloaded
-        maven {
-            url = uri("https://maven.pkg.github.com/Karthikeyan8296/dependencyDoctor")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                    ?: providers.gradleProperty("gpr.user").orNull
-                password = System.getenv("GH_PACKAGES_TOKEN")
-                    ?: providers.gradleProperty("gpr.token").orNull
-            }
-        }
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "CoverMe"
 include(":app")

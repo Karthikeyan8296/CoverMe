37fc44b7d86ce270b947fc86d6d3d7fb4c5da776
Karthik <karthikeyan.p@poshmark.com>
2026-04-02 14:29:35 +0530

    will work now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,10 +25,21 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/Karthikeyan8296/dependencyDoctor")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                    ?: providers.gradleProperty("gpr.user").orNull
+                password = System.getenv("GH_PACKAGES_TOKEN")
+                    ?: System.getenv("GITHUB_TOKEN")
+                            ?: providers.gradleProperty("gpr.token").orNull
+            }
+        }
         google()
         mavenCentral()
     }
 }
+
 
 rootProject.name = "CoverMe"
 include(":app")

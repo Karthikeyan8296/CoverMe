b54f7aae557a6554c8c92ce0f5de17cfecf20331
Karthik <karthikeyan.p@poshmark.com>
2026-04-07 10:46:03 +0530

    fixing the fix

--- app/src/main/java/com/example/coverme/MainActivity.kt
+++ app/src/main/java/com/example/coverme/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        Log.d("HELLO", "hello world")
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
